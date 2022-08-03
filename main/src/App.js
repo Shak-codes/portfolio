cdeebd078ea2620139565f89dc4c207c2186a84c
Shak12345 <shakirrahman81@gmail.com>
2022-08-03 00:26:49 -0400

    Added background svg

---
+++ main/src/App.js
@@ -0,0 +1,13 @@
+import bg from './bg.svg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+    <img src={bg} alt={'test'} className="bg"/>
+    
+    </div>
+  );
+}
+
+export default App;
